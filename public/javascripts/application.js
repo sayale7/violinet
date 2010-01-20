c9fe15f41f07b7fd86a9bdeb1c2f00516ccca824
sayale7 <thomas.kohler@gmx.com>
2010-01-20 13:39:46 +0100

    friendship changes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,50 +1,50 @@
 $(document).ready(function(){
-
-	
+ 
+ 
 	//hide normal textarea and textbox in posts
 	$('#post_form_area').removeClass('tb_area_extended').addClass('mceEditor');  
 	$('#post_form_text').removeClass('tb').addClass('tb_extended'); 
-	
+ 
 	//hide none js div for image gallery
 	$("#all_images").removeClass(".wjs_visible").addClass("wjs");
 	// $("all_images_slider").removeClass(".wjs").addClass("wjs_visible");
-	
+ 
 	$(".plain_old_checkboxes").remove();
-	
+ 
 	$(".new_click_boxes").removeClass("new_click_boxes");
-	
+ 
 	$(".clickable_items_to_add").live("click", function(){
 		var value = $(this).attr("id");
 		$(this).removeClass("clickable_items_to_add").addClass("clickable_items_selected_to_add");
 		$(this).append("<input type='hidden' name='tags[]' value='" + value + "'/>"); //value=' " +  $this.attr("id") + "'
 	});
-	
+ 
 	$(".clickable_items_selected_to_add").live("click", function(){
 		$(this).removeClass("clickable_items_selected_to_add").addClass("clickable_items_to_add");
 		$(this).children().remove();
 	});
-	
+ 
 	$(".clickable_items_to_remove").live("click", function(){
 		var value = $(this).attr("id");
 		$(this).removeClass("clickable_items_to_remove").addClass("clickable_items_selected_to_remove");
 		$(this).append("<input type='hidden' name='tags[]' value='" + value + "'/>"); //value=' " +  $this.attr("id") + "'
 	});
-	
+ 
 	$(".clickable_items_selected_to_remove").live("click", function(){
 		$(this).removeClass("clickable_items_selected_to_remove").addClass("clickable_items_to_remove");
 		$(this).children().remove();
 	});
-	
+ 
 	$("#edit_messages a").live("click", function(){
 		$("#edit_messages").html("loading ...");
 		$.getScript(this.href);
 	  	return false;
 	});
-	
-
-	
+ 
+ 
+ 
 	// toggle comment text area
-	
+ 
 	$("#new_comment").hide();
 	$("#new_comment_link").css("visibility","visible");
 	$(".close").css("visibility","visible");
@@ -57,39 +57,39 @@ $(document).ready(function(){
 		$("#new_comment_link").fadeIn('fast');
 		return false;
 	});
-	
-
-
+ 
+ 
+ 
 	$("#add_tags").live("submit", function(){
-		// $("#not_tags_of_post").html("loading ...");
-		// $("#tags_of_post").html("");
 		$.post($(this).attr("action"), $(this).serialize(), null, 'script');
-	  	return false;
-	});
-	
-	$("#remove_tags").live("submit", function(){
 		$("#not_tags_of_post").html("loading ...");
 		$("#tags_of_post").html("");
-		$.post($(this).attr("action"), $(this).serialize(), null, 'script');
-	  	return false;
+		return false;
 	});
-	
+
+	$("#remove_tags").live("submit", function(){
+		$.post($(this).attr("action"), $(this).serialize(), null, 'script');
+		$("#not_tags_of_post").html("loading ...");
+		$("#tags_of_post").html("");
+		return false;
+	});
+ 
 	// post new comment through ajax
-	
+ 
 	$("#new_comment").live("submit", function(){
 		$(".loading").html("loading ...");
 		$.post($(this).attr("action"), $(this).serialize(), null, 'script');
 	  	return false;
 	});
-	
+ 
 	// destroy comment through ajax
-	
+ 
 	$(".destroy_comment").live("click", function(){
 		$(".loading").html("loading ...");
 		$.getScript(this.href);
 		return false;
 	});
-
-
-	
+ 
+ 
+ 
 });
