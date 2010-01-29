c91d521fca560a7939fa9b30a32737619a176d33
sayale7 <thomas.kohler@gmx.com>
2010-01-29 16:47:37 +0100

    login welcome

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,10 +11,6 @@ class Event < ActiveRecord::Base
   has_attached_file :flyer, :styles => { :small => '300x300#' },
                     :url  => "/assets/events/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/events/:id/:style/:basename.:extension"
-  
-  validates_attachment_presence :flyer
-  validates_attachment_size :flyer, :less_than => 500.kilobytes
-  validates_attachment_content_type :flyer, :content_type => 'image/jpeg'
         
   private
   
