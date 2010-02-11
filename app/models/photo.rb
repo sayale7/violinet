000eea3f5ec0d770108cdbd7d0ba207aff680b61
sayale7 <thomas.kohler@gmx.com>
2010-02-11 11:38:06 +0100

    photo_album photo size

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,12 +5,10 @@ class Photo < ActiveRecord::Base
   has_attachment :content_type => :image,
                  :storage => :file_system,
                  :size => 1 .. 5000.kilobytes,
-                 :thumbnails => {:thumb => '75x75!', :medium  => '400x320!', :large => '500x500>' },
+                 :thumbnails => {:thumb => '75x75!', :large => '500x500>', :max  => '1000x1000>' },
                  :processor => :MiniMagick
                  
   validates_as_attachment
-  
-  
   
   before_thumbnail_saved do |thumbnail|
     image = Photo.find_by_id(thumbnail.parent_id)
