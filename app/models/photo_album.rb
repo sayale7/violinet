54ec9fa2efc307755e4f51ceaff5ebd1efff3f92
sayale7 <thomas.kohler@gmx.com>
2010-02-18 12:03:30 +0100

    job link tag

--- app/models/photo_album.rb
+++ app/models/photo_album.rb
@@ -3,6 +3,8 @@ class PhotoAlbum < ActiveRecord::Base
   belongs_to :user
   has_many :photos, :dependent  => :destroy
   
+  validates_presence_of :name
+  
   validates_length_of :description, :maximum=>2000
   
 end
