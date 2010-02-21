25d6ae9e93cc6ed136fca6be7bf5da8844fe34ee
sayale7 <thomas.kohler@gmx.com>
2010-02-21 11:27:32 +0100

    loading_background for pagination links

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,7 +1,8 @@
 $(document).ready(function() { 
 	$('.pagination a').live("click", function () {  
     	$.setFragment({ page: $.queryString(this.href).page });
-		$(".pagination").html("loading ...");
+		$('#loading_background').removeClass('loading_background_invisible');
+		$('#loading_background').addClass('loading_background_visible');
 	  	return false;
 	});
 
