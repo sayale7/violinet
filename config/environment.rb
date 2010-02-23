3fbf6b4ce390ae2095f9f655587d2e0407ff1b77
sayale7 <thomas.kohler@gmx.com>
2010-02-23 13:38:04 +0100

    environment.rb remove kete-tiny_mce

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,7 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :version => '~> 2.3.8', :lib => 'will_paginate', 
   :source => 'http://gems.github.com'
   config.gem "calendar_date_select"
-  config.gem "kete-tiny_mce", :lib => "tiny_mce"
+  #config.gem "kete-tiny_mce", :lib => "tiny_mce"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
