bdf9ddefc517810eb187ed244e7e391896061049
sayale7 <thomas.kohler@gmx.com>
2010-03-05 13:43:05 +0100

    bildergallerie

--- public/javascripts/popup.js
+++ public/javascripts/popup.js
@@ -3,38 +3,36 @@
 //0 means disabled; 1 means enabled;
 var popupStatus = 0;
 //loading popup with jQuery magic!
-function loadPopup(){
+function loadPopup(element){
 	//loads popup only if it is disabled
 	if (popupStatus == 0) {
 		$("#backgroundPopup").css({
 			"opacity": "0.9"
 		});
 		$("#backgroundPopup").fadeIn("fast");
-		$("#popupContact").fadeIn("fast");
+		$('#'+ element).fadeIn("fast");
 
 		popupStatus = 1;
 	}
 }
 
 //disabling popup with jQuery magic!
-function disablePopup(){
+function disablePopup(element){
 	//disables popup only if it is enabled
-	if (popupStatus == 1) {
-		$("#backgroundPopup").fadeOut("fast");
-		$("#popupContact").fadeOut("fast");
-		popupStatus = 0;
-	}
+	$("#backgroundPopup").fadeOut("fast");
+	$('#'+ element).fadeOut("fast");
+	popupStatus = 0;
 }
 
 //centering popup
-function centerPopup(){
+function centerPopup(element){
 	//request data for centering
 	var windowWidth = document.documentElement.clientWidth;
 	var windowHeight = document.documentElement.clientHeight;
-	var popupHeight = $("#popupContact").height();
-	var popupWidth = $("#popupContact").width();
+	var popupHeight = $('#'+ element).height();
+	var popupWidth = $('#'+ element).width();
 	//centering
-	$("#popupContact").css({
+	$('#'+ element).css({
 		"position": "absolute",
 		"top": windowHeight / 2 - popupHeight / 2,
 		"left": windowWidth / 2 - popupWidth / 2
@@ -52,20 +50,21 @@ $(document).ready(function(){
 
 	//LOADING POPUP
 	//Click the button event!
-	$("#button_popup, .popup").live('click', function(){
+	$(".popup").live('click', function(){
 		//centering with css
+		
 		$('.id_of_photo').attr('action', '/photos/'+ $(this).attr("id"));
 		$('.id_of_photo').find('.tb_area').attr('value', $(this).attr("title"));
-		centerPopup();
+		centerPopup($(this).attr('rel'));
 		//load popup
-		loadPopup();
+		loadPopup($(this).attr('rel'));
 		return false;
 	});
 
 	//CLOSING POPUP
 	//Click the x event!
-	$("#popupContactClose").click(function(){
-		disablePopup();
+	$("#popupContactClose").live('click', function(){
+		disablePopup($(this).attr('rel'));
 		return false;
 	});
 	//Click out event!
